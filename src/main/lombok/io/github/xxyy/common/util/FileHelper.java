e5af1f1e2658b6550c85c623ff4953379cf6bf8e
xxyy <devnull@nowak-at.net>
2014-04-11 21:11:45 +0200

    Create files if they don't exist while copying in FileHelper

--- src/main/lombok/io/github/xxyy/common/util/FileHelper.java
+++ src/main/lombok/io/github/xxyy/common/util/FileHelper.java
@@ -1,11 +1,6 @@
 package io.github.xxyy.common.util;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,59 +10,60 @@ import java.nio.file.attribute.BasicFileAttributes;
 /**
  * Class that provides some static utilities to work
  * with files.
- * 
+ *
  * @author <a href="http://xxyy.github.io/">xxyy</a>
  */
-public abstract class FileHelper
-{
+public abstract class FileHelper {
     /**
      * Copies a file or directory {@code src} (and all its contents, if any)
      * to a new destination {@code dest}.
+     *
      * @throws IOException If something went wrong while copying
      */
-    public static void copyFolder(File src, File dest) throws IOException{
-        if(src.isDirectory())
-        {
-            if(!dest.exists())
-            {
-               assert dest.mkdirs();
+    public static void copyFolder(File src, File dest) throws IOException {
+        if (src.isDirectory()) {
+            if (!dest.exists()) {
+                assert dest.mkdirs();
             }
-     
+
             String files[] = src.list();
-     
-            for (String file : files) 
-            {
-               File srcFile = new File(src, file);
-               File destFile = new File(dest, file);
-               FileHelper.copyFolder(srcFile,destFile);
+
+            for (String file : files) {
+                File srcFile = new File(src, file);
+                File destFile = new File(dest, file);
+                FileHelper.copyFolder(srcFile, destFile);
             }
-     
-        }
-        else
-        {
-            InputStream in = new FileInputStream(src);
-            OutputStream out = new FileOutputStream(dest); 
-            byte[] buf = new byte[1024];
- 
-            int length;
-            while ((length = in.read(buf)) > 0)
-            {
-               out.write(buf, 0, length);
+
+        } else {
+            if (!dest.exists()) {
+                assert dest.createNewFile();
             }
- 
-            in.close();
-            out.close();
+
+            try (InputStream in = new FileInputStream(src);
+                 OutputStream out = new FileOutputStream(dest)) {
+
+                byte[] buf = new byte[1024];
+
+                int length;
+                while ((length = in.read(buf)) > 0) {
+                    out.write(buf, 0, length);
+                }
+
+                in.close();
+                out.close();
+            }
         }
     }
-    
+
     /**
      * Deletes a file or directory and all its contents.
+     *
      * @param pathToDir a {@link Path} pointing to the directory to be deleted.
      * @throws java.io.IOException If a file could not be deleted.
      */
-    public static void deleteAll(Path pathToDir) throws IOException{
+    public static void deleteAll(Path pathToDir) throws IOException {
         Files.walkFileTree(pathToDir, new SimpleFileVisitor<Path>() {
-            
+
             @Override
             public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
                 if (exc == null) {
@@ -76,13 +72,13 @@ public abstract class FileHelper
                 }
                 throw exc;
             }
-       
+
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 Files.delete(file);
                 return FileVisitResult.CONTINUE;
             }
-       
+
         });
     }
 }
