a00bed553cabf3a5fdbc3a1e151c43fbc78c2578
xxyy <devnull@nowak-at.net>
2014-05-09 00:20:20 +0200

    Bukkit simplified their Scoreboard API, Remove antrun in favor of clean.

--- src/main/lombok/io/github/xxyy/common/util/ScoreboardHelper.java
+++ src/main/lombok/io/github/xxyy/common/util/ScoreboardHelper.java
@@ -1,17 +1,8 @@
 package io.github.xxyy.common.util;
 
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Objective;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * Provides some static utility methods for dealing with Scoreboards.
@@ -21,113 +12,30 @@ import java.util.UUID;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ScoreboardHelper {
-    private static final Map<String, NamedOfflinePlayer> CACHED_OFFLINE_PLAYERS = new HashMap<>();
-
     /**
-     * Sets a fake score with a specified display name. Does not call {@link org.bukkit.Bukkit#getOfflinePlayer(String)}, but creates
-     * a fake {@link io.github.xxyy.common.util.ScoreboardHelper.NamedOfflinePlayer} to save performance.
-     * @param objective Objective to set
+     * Sets a fake score with a specified display name. Does not call {@link org.bukkit.Bukkit#getOfflinePlayer(String)}.
+     *
+     * @param objective   Objective to set
      * @param displayName Name to display
-     * @param score Score to set
-     * @param addToCache Whether to store the created {@link io.github.xxyy.common.util.ScoreboardHelper.NamedOfflinePlayer} in a static data structure, to
-     *                   prevent having to create new ones - this is especially helpful for multiple Scoreboards/Objectives using the same display name
-     *                   for all players.
+     * @param score       Score to set
+     * @param addToCache  Kept for compatibility with old code. Ignored.
+     * @deprecated Unused parameter. Kept for compatibility with existing code - Use
+     * {@link #setFakeScore(org.bukkit.scoreboard.Objective, String, int)}.
      */
+    @Deprecated
     public static void setFakeScore(final Objective objective, final String displayName, final int score, final boolean addToCache) {
-        NamedOfflinePlayer fakePlayer = CACHED_OFFLINE_PLAYERS.get(displayName);
-
-        if(fakePlayer == null){
-            fakePlayer = new NamedOfflinePlayer(displayName);
-
-            if(addToCache){
-                CACHED_OFFLINE_PLAYERS.put(displayName, fakePlayer);
-            }
-        }
-
-        objective.getScore(fakePlayer).setScore(score);
+        objective.getScore(displayName).setScore(score);
     }
 
     /**
-     * Represents a fake OfflinePlayer - This is mainly intended for use with Scoreboards.
-     * All methods but {@link #getName()} throw an {@link java.lang.UnsupportedOperationException}, do nothing or return {@code null} (depends on method),
-     * therefore these objects should only be passed to methods which just use {@link #getName()} (or extend and implement).
+     * Sets a fake score with a specified display name. Does not call {@link org.bukkit.Bukkit#getOfflinePlayer(String)}.
+     *
+     * @param objective   Objective to set
+     * @param displayName Name to display
+     * @param score       Score to set
      */
-    @RequiredArgsConstructor
-    public static class NamedOfflinePlayer implements OfflinePlayer {
-        private static final UnsupportedOperationException UNSUPPORTED_OPERATION_EXCEPTION = //Store the exception because it's always the same and stuff
-                new UnsupportedOperationException("This fake OfflinePlayer was just created to return a name," +
-                        " to trick Bukkit in order to save performance (Thnx, EvilSeph) - It only supports getName().");
-        @Getter
-        private final String name;
-
-        @Override
-        public boolean isOnline() {
-            return false;
-        }
-
-        @Override
-        public UUID getUniqueId() {
-            throw UNSUPPORTED_OPERATION_EXCEPTION;
-        }
-
-        @Override
-        public boolean isBanned() {
-            return false; //Ignore this, won't hurt
-        }
-
-        @Override @Deprecated
-        public void setBanned(boolean b) {
-            //Ignore this, won't hurt
-        }
-
-        @Override
-        public boolean isWhitelisted() {
-            throw UNSUPPORTED_OPERATION_EXCEPTION;
-        }
-
-        @Override
-        public void setWhitelisted(boolean b) {
-            throw UNSUPPORTED_OPERATION_EXCEPTION;
-        }
-
-        @Override
-        public Player getPlayer() {
-            return null; //Return null to possibly trick implementations into believing the player is just offline
-        }
-
-        @Override
-        public long getFirstPlayed() {
-            throw UNSUPPORTED_OPERATION_EXCEPTION;
-        }
-
-        @Override
-        public long getLastPlayed() {
-            throw UNSUPPORTED_OPERATION_EXCEPTION;
-        }
-
-        @Override
-        public boolean hasPlayedBefore() {
-            return false;
-        }
-
-        @Override
-        public Location getBedSpawnLocation() {
-            throw UNSUPPORTED_OPERATION_EXCEPTION;
-        }
-
-        @Override
-        public Map<String, Object> serialize() {
-            throw UNSUPPORTED_OPERATION_EXCEPTION;
-        }
-
-        @Override
-        public boolean isOp() {
-            throw UNSUPPORTED_OPERATION_EXCEPTION;
-        }
-
-        @Override
-        public void setOp(boolean b) {
-            //Ignore this, won't hurt
-        }
+    public static void setFakeScore(final Objective objective, final String displayName, final int score) {
+        //noinspection deprecation
+        setFakeScore(objective, displayName, score, false);
     }
 }
