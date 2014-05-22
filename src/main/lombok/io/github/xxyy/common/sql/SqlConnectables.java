dab0db23c44cf70234bab50c9e9355ae2b200443
xxyy <devnull@nowak-at.net>
2014-05-22 23:00:06 +0200

    Add more utility methods for host strings to SqlConnectables

--- src/main/lombok/io/github/xxyy/common/sql/SqlConnectables.java
+++ src/main/lombok/io/github/xxyy/common/sql/SqlConnectables.java
@@ -28,8 +28,20 @@ public final class SqlConnectables {
 
         Validate.notNull(sqlHost);
 
-        return sqlHost.contains(connectable.getSqlDb()) ? sqlHost : ((sqlHost.endsWith("/")) ? sqlHost + connectable.
-                getSqlDb() : sqlHost + "/" + connectable.getSqlDb());
+        return getHostString(connectable.getSqlDb(), sqlHost);
+    }
+
+    /**
+     * Returns the host string for the given paramaeters. The host string consists of
+     * the database URL, as returned by {@link SqlConnectable#getSqlHost()}, of a slash (/)
+     * and the database name, as returned by {@link SqlConnectable#getSqlDb()}.
+     * If the database name is already appended to the host, it will not be appended another time.
+     * @param database Database to use
+     * @param sqlHost Fully-qualified JDBC connection string of the host to use
+     * @return Host string, as accepted by database drivers.
+     */
+    public static String getHostString(String database, String sqlHost) {
+        return sqlHost.contains(database) ? sqlHost : (sqlHost.endsWith("/") ? sqlHost + database : sqlHost + "/" + database);
     }
 
     /**
@@ -64,5 +76,27 @@ public final class SqlConnectables {
                 return user;
             }
         };
+    }
+
+    /**
+     * Returns the host string for the given paramaeters. The host string consists of
+     * the database URL, as returned by {@link SqlConnectable#getSqlHost()}, of a slash (/)
+     * and the database name, as returned by {@link SqlConnectable#getSqlDb()}.
+     * If the database name is already appended to the host, it will not be appended another time.
+     * @param database Database to use
+     * @param hostname Fully-qualified JDBC connection string of the host to use or just a FQDN.
+     * @param port port number to use.
+     * @return Host string, as accepted by database drivers.
+     */
+    public static String getHostString(String hostname, int port, String database) {
+        String sqlHost;
+
+        if(!hostname.startsWith("jdbc:")) {
+            sqlHost = "jdbc:mysql://" + hostname + ":" + port + "/";
+        } else {
+            sqlHost = hostname;
+        }
+
+        return getHostString(sqlHost, database);
     }
 }
