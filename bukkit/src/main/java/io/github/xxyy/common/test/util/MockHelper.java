fd0d94a2846ac823c8c59e51f05e89e4ef67e3ba
xxyy <devnull@nowak-at.net>
2014-08-20 00:27:56 +0200

    (3.0.2) Remove xyc-bukkit-test in favor of having the single class in xyc-bukkit

--- bukkit/src/main/java/io/github/xxyy/common/test/util/MockHelper.java
+++ bukkit/src/main/java/io/github/xxyy/common/test/util/MockHelper.java
@@ -13,17 +13,12 @@ import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.UUID;
 import java.util.logging.Logger;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 
 /**
  * @author <a href="http://xxyy.github.io/">xxyy</a>
@@ -39,19 +34,19 @@ public class MockHelper {
         Server server = Bukkit.getServer();
 
         if (server == null) {
-            server = mock(Server.class);
+            server = Mockito.mock(Server.class);
         } else {
-            reset(server);
+            Mockito.reset(server);
         }
 
-        when(server.getName()).thenReturn("Spagt");
-        when(server.getBukkitVersion()).thenReturn("fuk of bukite");
-        when(server.getVersion()).thenReturn("infinity");
-        when(server.getLogger()).thenReturn(Logger.getLogger(Server.class.getName()));
+        Mockito.when(server.getName()).thenReturn("Spagt");
+        Mockito.when(server.getBukkitVersion()).thenReturn("fuk of bukite");
+        Mockito.when(server.getVersion()).thenReturn("infinity");
+        Mockito.when(server.getLogger()).thenReturn(Logger.getLogger(Server.class.getName()));
 
-        CommandSender consoleSender = loggerSender(mock(ConsoleCommandSender.class), Bukkit.getServer().getLogger());
-        when(server.getConsoleSender()).thenAnswer(invocation -> consoleSender);
-        when(server.getPlayer(any(UUID.class))).then(id -> Arrays.asList(Bukkit.getServer().getOnlinePlayers()).stream()
+        CommandSender consoleSender = loggerSender(Mockito.mock(ConsoleCommandSender.class), Bukkit.getServer().getLogger());
+        Mockito.when(server.getConsoleSender()).thenAnswer(invocation -> consoleSender);
+        Mockito.when(server.getPlayer(Matchers.any(UUID.class))).then(id -> Arrays.asList(Bukkit.getServer().getOnlinePlayers()).stream()
                 .filter(plr -> plr.getUniqueId().equals(id.getArguments()[0]))
                 .findAny().orElse(null));
 
@@ -63,34 +58,34 @@ public class MockHelper {
     }
 
     public static Player mockPlayer(final UUID uuid, final String name) {
-        Player plr = mock(Player.class);
-        when(plr.getUniqueId()).thenReturn(uuid);
-        when(plr.getName()).thenReturn(name);
+        Player plr = Mockito.mock(Player.class);
+        Mockito.when(plr.getUniqueId()).thenReturn(uuid);
+        Mockito.when(plr.getName()).thenReturn(name);
         return plr;
     }
 
     public static CommandSender printlnSender(CommandSender sender) {
         if (!Mockito.mockingDetails(sender).isMock()) {
-            sender = spy(sender);
+            sender = Mockito.spy(sender);
         }
 
         Mockito.doAnswer((invocation) -> {
             System.out.println(invocation.getArguments()[0]);
             return null;
-        }).when(sender).sendMessage(any(String.class));
+        }).when(sender).sendMessage(Matchers.any(String.class));
 
         return sender;
     }
 
     public static CommandSender loggerSender(CommandSender sender, Logger logger) {
         if (!Mockito.mockingDetails(sender).isMock()) {
-            sender = spy(sender);
+            sender = Mockito.spy(sender);
         }
 
         Mockito.doAnswer((invocation) -> {
             logger.info((String) invocation.getArguments()[0]);
             return null;
-        }).when(sender).sendMessage(any(String.class));
+        }).when(sender).sendMessage(Matchers.any(String.class));
 
         return sender;
     }
