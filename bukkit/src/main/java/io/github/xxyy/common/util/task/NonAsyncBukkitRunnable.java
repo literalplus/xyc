c80e414b25413663cc272131ebb76a0b33ad5a86
xxyy <devnull@nowak-at.net>
2014-12-30 19:32:55 +0100

    Update coypright year

--- bukkit/src/main/java/io/github/xxyy/common/util/task/NonAsyncBukkitRunnable.java
+++ bukkit/src/main/java/io/github/xxyy/common/util/task/NonAsyncBukkitRunnable.java
@@ -1,9 +1,11 @@
 /*
- * Copyright (c) 2013 - 2014 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
  *
  * Any usage, including, but not limited to, compiling, running, redistributing, printing,
  *  copying and reverse-engineering is strictly prohibited without explicit written permission
  *  from the original author and may result in legal steps being taken.
+ *
+ * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
  */
 
 package io.github.xxyy.common.util.task;
--- games/src/main/java/io/github/xxyy/common/xyplugin/SqlXyGamePlugin.java
+++ games/src/main/java/io/github/xxyy/common/xyplugin/SqlXyGamePlugin.java
@@ -4,6 +4,8 @@
  * Any usage, including, but not limited to, compiling, running, redistributing, printing,
  *  copying and reverse-engineering is strictly prohibited without explicit written permission
  *  from the original author and may result in legal steps being taken.
+ *
+ * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
  */
 
 package io.github.xxyy.common.xyplugin;
