c46334248aa574e0307329662b022b65a7db7ec6
xxyy <devnull@nowak-at.net>
2015-09-20 21:49:50 +0200

    Add username -> UUID at time to Mojang API wrapper
    That method is not supported by the original Mojang AccountsClient.

--- core/src/main/java/io/github/xxyy/common/lib/com/mojang/api/profiles/ProfileRepository.java
+++ core/src/main/java/io/github/xxyy/common/lib/com/mojang/api/profiles/ProfileRepository.java
@@ -1,5 +1,25 @@
+/*
+ * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ *
+ * Any usage, including, but not limited to, compiling, running, redistributing, printing,
+ *  copying and reverse-engineering is strictly prohibited without explicit written permission
+ *  from the original author and may result in legal steps being taken.
+ *
+ * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ */
+
 package io.github.xxyy.common.lib.com.mojang.api.profiles;
 
 public interface ProfileRepository {
-    public Profile[] findProfilesByNames(String... names);
+    Profile[] findProfilesByNames(String... names);
+
+    /**
+     * Finds the current (!) profile for a name at a specified time. So, for example, if the requested player was
+     * named Albert at the given time, but has since changed their name to Berta, the returned profile will contain
+     * the current name. (Berta, in that case)
+     * @param name the name to request the profile for
+     * @param unixTime the timestamp, in seconds since the begin of the Unix epoch
+     * @return the profile, if found
+     */
+    Profile findProfileAtTime(String name, long unixTime);
 }
