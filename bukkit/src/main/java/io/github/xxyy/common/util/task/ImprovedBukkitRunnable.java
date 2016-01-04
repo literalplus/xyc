9d0be66e7e78ce95d13594d34a9a67cb40b9c797
xxyy <devnull@nowak-at.net>
2016-01-04 01:04:19 +0100

    Make ImprovedBukkitRunnable no longer inherit BukkitRunnable
    This removed the idiotic problem of having the thing throw exceptions
     all over the place. Also it can now be rescheduled.
    (cherry picked from commit d7c88ba)

--- bukkit/src/main/java/io/github/xxyy/common/util/task/ImprovedBukkitRunnable.java
+++ bukkit/src/main/java/io/github/xxyy/common/util/task/ImprovedBukkitRunnable.java
@@ -10,27 +10,164 @@
 
 package io.github.xxyy.common.util.task;
 
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scheduler.BukkitTask;
 
 /**
- * Adds some extra functionality to {@link org.bukkit.scheduler.BukkitRunnable}.
+ * A less limited clone of {@link org.bukkit.scheduler.BukkitRunnable}.
  *
  * @author <a href="http://xxyy.github.io/">xxyy</a>
  * @since 20.7.14
  */
-public abstract class ImprovedBukkitRunnable extends BukkitRunnable {
+public abstract class ImprovedBukkitRunnable implements Runnable {
+    private int taskId = -1;
+
+    /**
+     * Attempts to cancel this task.
+     *
+     * @throws IllegalStateException if task was not scheduled yet
+     */
+    public synchronized void cancel() throws IllegalStateException {
+        checkState();
+        tryCancel();
+    }
+
+    /**
+     * Schedules this in the Bukkit scheduler to run on next tick.
+     *
+     * @param plugin the reference to the plugin scheduling task
+     * @return a BukkitTask that contains the id number
+     * @throws IllegalArgumentException if plugin is null
+     * @throws IllegalStateException if this was already scheduled
+     * @see BukkitScheduler#runTask(Plugin, Runnable)
+     */
+    public synchronized BukkitTask runTask(Plugin plugin) throws IllegalArgumentException, IllegalStateException {
+        checkState();
+        return setupId(Bukkit.getScheduler().runTask(plugin, this));
+    }
+
+    /**
+     * <b>Asynchronous tasks should never access any API in Bukkit. Great care
+     * should be taken to assure the thread-safety of asynchronous tasks.</b>
+     * <p>
+     * Schedules this in the Bukkit scheduler to run asynchronously.
+     *
+     * @param plugin the reference to the plugin scheduling task
+     * @return a BukkitTask that contains the id number
+     * @throws IllegalArgumentException if plugin is null
+     * @throws IllegalStateException if this was already scheduled
+     * @see BukkitScheduler#runTaskAsynchronously(Plugin, Runnable)
+     */
+    public synchronized BukkitTask runTaskAsynchronously(Plugin plugin) throws IllegalArgumentException, IllegalStateException  {
+        checkState();
+        return setupId(Bukkit.getScheduler().runTaskAsynchronously(plugin, this));
+    }
+
+    /**
+     * Schedules this to run after the specified number of server ticks.
+     *
+     * @param plugin the reference to the plugin scheduling task
+     * @param delay the ticks to wait before running the task
+     * @return a BukkitTask that contains the id number
+     * @throws IllegalArgumentException if plugin is null
+     * @throws IllegalStateException if this was already scheduled
+     * @see BukkitScheduler#runTaskLater(Plugin, Runnable, long)
+     */
+    public synchronized BukkitTask runTaskLater(Plugin plugin, long delay) throws IllegalArgumentException, IllegalStateException  {
+        checkState();
+        return setupId(Bukkit.getScheduler().runTaskLater(plugin, this, delay));
+    }
+
+    /**
+     * <b>Asynchronous tasks should never access any API in Bukkit. Great care
+     * should be taken to assure the thread-safety of asynchronous tasks.</b>
+     * <p>
+     * Schedules this to run asynchronously after the specified number of
+     * server ticks.
+     *
+     * @param plugin the reference to the plugin scheduling task
+     * @param delay the ticks to wait before running the task
+     * @return a BukkitTask that contains the id number
+     * @throws IllegalArgumentException if plugin is null
+     * @throws IllegalStateException if this was already scheduled
+     * @see BukkitScheduler#runTaskLaterAsynchronously(Plugin, Runnable, long)
+     */
+    public synchronized BukkitTask runTaskLaterAsynchronously(Plugin plugin, long delay) throws IllegalArgumentException, IllegalStateException  {
+        checkState();
+        return setupId(Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, this, delay));
+    }
+
+    /**
+     * Schedules this to repeatedly run until cancelled, starting after the
+     * specified number of server ticks.
+     *
+     * @param plugin the reference to the plugin scheduling task
+     * @param delay the ticks to wait before running the task
+     * @param period the ticks to wait between runs
+     * @return a BukkitTask that contains the id number
+     * @throws IllegalArgumentException if plugin is null
+     * @throws IllegalStateException if this was already scheduled
+     * @see BukkitScheduler#runTaskTimer(Plugin, Runnable, long, long)
+     */
+    public synchronized BukkitTask runTaskTimer(Plugin plugin, long delay, long period) throws IllegalArgumentException, IllegalStateException  {
+        checkState();
+        return setupId(Bukkit.getScheduler().runTaskTimer(plugin, this, delay, period));
+    }
+
+    /**
+     * <b>Asynchronous tasks should never access any API in Bukkit. Great care
+     * should be taken to assure the thread-safety of asynchronous tasks.</b>
+     * <p>
+     * Schedules this to repeatedly run asynchronously until cancelled,
+     * starting after the specified number of server ticks.
+     *
+     * @param plugin the reference to the plugin scheduling task
+     * @param delay the ticks to wait before running the task for the first
+     *     time
+     * @param period the ticks to wait between runs
+     * @return a BukkitTask that contains the id number
+     * @throws IllegalArgumentException if plugin is null
+     * @throws IllegalStateException if this was already scheduled
+     * @see BukkitScheduler#runTaskTimerAsynchronously(Plugin, Runnable, long,
+     *     long)
+     */
+    public synchronized BukkitTask runTaskTimerAsynchronously(Plugin plugin, long delay, long period) throws IllegalArgumentException, IllegalStateException  {
+        checkState();
+        return setupId(Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, this, delay, period));
+    }
+
+    /**
+     * Gets the task id for this runnable, or -1 if the runnable has never been scheduled.
+     *
+     * @return the task id that this runnable was scheduled as, or -1
+     */
+    public synchronized int getTaskId() throws IllegalStateException {
+        return taskId;
+    }
+
+    private void checkState() {
+        if (taskId != -1) {
+            throw new IllegalStateException("Already scheduled as " + taskId);
+        }
+    }
+
+    private BukkitTask setupId(final BukkitTask task) {
+        this.taskId = task.getTaskId();
+        return task;
+    }
+
     /**
      * Cancels this task if it's scheduled.
      * Does not throw an exception if it's not scheduled.
      *
      * @see #cancel()
      */
-    public void tryCancel() {
-        try {
-            this.cancel();
-        } catch(IllegalStateException ignore) {
-            //Bukkit won't even return a -1 task id - They'll just throw this and nothing to do about it
-            //This is totally how you API properly
+    public synchronized void tryCancel() {
+        if(taskId != -1) {
+            Bukkit.getScheduler().cancelTask(taskId);
+            taskId = -1;
         }
     }
 }
