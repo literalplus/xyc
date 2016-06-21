literalplus/xyc

commit ba57271063827165e08963a9ba22fdb578f5fb10

[BREAKING] Rework Kit API
This commit reworks the Kit API to work with File objects instead of
String paths and UUIDs instead of player names and then I just kind
of refactored the whole thing. Sorry not sorry.

I could show you incredible refactorings

games/src/main/java/io/github/xxyy/common/games/kits/Kit.java
@@ -1,0 +1,264 @@
+/*
+ * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ *
+ * Any usage, including, but not limited to, compiling, running, redistributing, printing,
+ *  copying and reverse-engineering is strictly prohibited without explicit written permission
+ *  from the original author and may result in legal steps being taken.
+ *
+ * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ */
+
+package io.github.xxyy.common.games.kits;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import io.github.xxyy.common.util.inventory.InventoryHelper;
+
+import java.io.File;
+
+/**
+ * Represents a kit that may be applied to a player's inventory and armor and is backed by a YAML
+ * file.
+ *
+ * @author <a href="http://xxyy.github.io/">xxyy</a>
+ */
+public class Kit implements Comparable<Kit> {
+    public static final String ID_PATH = "id";
+    public static final String ICON_PATH = "icon";
+    public static final String ARMOR_PATH = "armor";
+    public static final String CONTENTS_PATH = "contents";
+    public static final String OBJECTIVE_PATH = "objective";
+    public static final String AUTHOR_PATH = "author";
+
+    private final KitManager manager;
+    private final String name;
+    private final File file;
+    private ItemStack iconStack;
+    private ItemStack[] contents = new ItemStack[InventoryHelper.PLAYER_INV_SIZE];
+    private ItemStack[] armor = new ItemStack[InventoryHelper.ARMOR_SIZE];
+    private String objective;
+    private int id; //To make sure kits are displayed in order
+
+    private String authorName;
+    private YamlConfiguration config;
+
+    /**
+     * Creates a new kit.
+     *
+     * @param manager    the manager associated with this kit
+     * @param id         the sort id of this kit (higher numbers displayed last)
+     * @param file       the file where this kit is stored
+     * @param iconStack  the icon stack used to represent this kit in inventories
+     * @param contents   the inventory contents for this kit
+     * @param armor      the armor contents for this kit
+     * @param objective  the objective string for this kit, null for open kits
+     * @param authorName the name of the player who created this kit
+     * @param config     the configuration object for this kit
+     */
+    Kit(KitManager manager, int id, File file, ItemStack iconStack,
+        ItemStack[] contents, ItemStack[] armor, String objective, String authorName,
+        YamlConfiguration config) {
+        this.manager = manager;
+        this.id = id;
+        this.file = file;
+        this.name = file.getName().replace(KitLoader.FILE_EXTENSION, "");
+        this.iconStack = iconStack;
+        this.contents = contents;
+        this.armor = armor;
+        this.objective = objective;
+        this.authorName = authorName;
+        this.config = config;
+    }
+
+    /**
+     * @return the kit manager associated with this kit
+     */
+    public KitManager getManager() {
+        return manager;
+    }
+
+    /**
+     * Returns the sort id of this kit. This is used so that kits are always displayed in the
+     * same order, which is useful if there are objective-locked kits that follow some kind of
+     * pattern. Higher numbers are displayed last. If two kits have the same sort id, they are
+     * sorted by their names.
+     *
+     * @return the sort id of this kit (higher numbers displayed last)
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the sort id for this kit. ({@link #getId() explanation})
+     *
+     * @param id the sort id of this kit (higher numbers displayed last)
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the file where this kit is stored
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * @return the objective string for this kit, null for open kits (e.g. no objective required)
+     * @see io.github.xxyy.common.games.kits.objective.ObjectiveResolver
+     */
+    public String getObjective() {
+        return objective;
+    }
+
+    /**
+     * Sets the objective string for this kit. To make this kit open, e.g. make it accessible
+     * without condition, set objective to null.
+     *
+     * @param objective the objective string for this kit
+     * @see io.github.xxyy.common.games.kits.objective.ObjectiveResolver
+     */
+    public void setObjective(String objective) {
+        this.objective = objective;
+    }
+
+    /**
+     * @return whether access to this kit is bound to an objective
+     */
+    public boolean hasObjective() {
+        return objective != null;
+    }
+
+    /**
+     * @return the armor contents for this kit
+     */
+    public ItemStack[] getArmor() {
+        return this.armor;
+    }
+
+    /**
+     * Sets the armor contents for this kit
+     *
+     * @param armor the armor contents for this kit
+     */
+    public void setArmor(ItemStack[] armor) {
+        this.armor = armor;
+    }
+
+    /**
+     * @return the name of the player who created this kit
+     */
+    public String getAuthorName() {
+        return this.authorName;
+    }
+
+    /**
+     * Sets the name of the player who created this kit.
+     *
+     * @param authorName the name of the player who created this kit
+     */
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    /**
+     * @return the configuration backing this kit
+     */
+    public YamlConfiguration getConfig() {
+        return this.config;
+    }
+
+    /**
+     * @return the inventory contents for this kit
+     */
+    public ItemStack[] getContents() {
+        return this.contents;
+    }
+
+    /**
+     * Sets the inventory contents for this kit
+     *
+     * @param contents the new inventory contents for this kit
+     */
+    public void setContents(ItemStack[] contents) {
+        this.contents = contents;
+    }
+
+    /**
+     * @return the icon that represents this kit in inventories
+     */
+    public ItemStack getIconStack() {
+        return this.iconStack;
+    }
+
+    /**
+     * Sets the icon that represents this kit in inventories
+     *
+     * @param iconStack the new icon that represents this kit in inventories
+     */
+    public void setIconStack(ItemStack iconStack) {
+        this.iconStack = iconStack;
+    }
+
+    /**
+     * @return the file name of this kit, excluding {@link KitLoader#FILE_EXTENSION file extension}
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.id;
+        result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
+        return result;
+    }
+
+    @Override
+    public int compareTo(@NotNull Kit other) {
+        if (this.equals(other)) {
+            return 0;
+        }
+        if (this.id == other.id) {
+            if (this.name.equalsIgnoreCase(other.name)) {
+                return 0;
+            }
+            return this.name.compareTo(other.name);
+        }
+        if (this.id < other.id) {
+            return -1;
+        }
+        return 1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Kit)) {
+            return false;
+        }
+        Kit other = (Kit) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!this.name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+}

games/src/main/java/io/github/xxyy/common/games/kits/objective/ParentObjectiveResolver.java
@@ -1,0 +1,125 @@
+/*
+ * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ *
+ * Any usage, including, but not limited to, compiling, running, redistributing, printing,
+ *  copying and reverse-engineering is strictly prohibited without explicit written permission
+ *  from the original author and may result in legal steps being taken.
+ *
+ * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ */
+
+package io.github.xxyy.common.games.kits.objective;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Resolves objectives by propagating calls to child objective resolvers using a special
+ * objective string format:
+ * <p>
+ * {@code objective type};;{@code objective parameters}
+ * </p>
+ * <p>
+ * The objective strings passed to child resolvers only contain the parameters to simplify
+ * parsing since every child resolver only resolves a single objective type. Note that objective
+ * types are case-sensitive.
+ * </p>
+ * <p>
+ * Access to unknown objective types is always denied.
+ * </p>
+ *
+ * @author <a href="http://xxyy.github.io/">xxyy</a>
+ * @since 2016-06-21
+ */
+public class ParentObjectiveResolver implements ObjectiveResolver {
+    public static final String TYPE_SEPARATOR = ";;";
+
+    // array indexes for objective format
+    private static final int TYPE = 0;
+    private static final int PARAM = 1;
+
+    private final Map<String, ObjectiveResolver> childResolvers = new HashMap<>();
+
+    /**
+     * Adds a child resolver for a specific objective type. See
+     * {@link ParentObjectiveResolver the class JavaDoc} for details. Existing resolvers for the
+     * same type will be replaced.
+     *
+     * @param type  the objective type the child resolves
+     * @param child the resolver for that type
+     * @return this resolver for call chaining
+     */
+    public ParentObjectiveResolver addChild(String type, ObjectiveResolver child) {
+        childResolvers.put(type, child);
+        return this;
+    }
+
+    /**
+     * Removes a child resolver by the handled objective type.
+     *
+     * @param type the type to remove the resolver for
+     * @return the removed resolver, or null if none was registered
+     */
+    public ObjectiveResolver removeChild(String type) {
+        return childResolvers.remove(type);
+    }
+
+    /**
+     * Gets the child resolver handling given objective type.
+     *
+     * @param type the type to get the resolver for
+     * @return the resolver for given type, or null if none was registered
+     */
+    public ObjectiveResolver getChild(String type) {
+        return childResolvers.get(type);
+    }
+
+    @Override
+    public boolean isKnown(String objective) {
+        return getChild(splitObjective(objective)[TYPE]) != null;
+    }
+
+    @Override
+    public boolean isCompleted(String objective, Player player) {
+        String[] args = splitObjective(objective);
+        String type = args[TYPE];
+        String parameters = args[PARAM];
+
+        ObjectiveResolver resolver = getChild(type);
+        //noinspection SimplifiableIfStatement
+        if (resolver == null) {
+            return false; //unknown objectives are denied
+        } else {
+            return resolver.isCompleted(parameters, player);
+        }
+    }
+
+    @Override
+    public String getName(String objective) {
+        String[] args = splitObjective(objective);
+        ObjectiveResolver resolver = getChild(args[TYPE]);
+        return resolver == null ? null : resolver.getName(args[PARAM]);
+    }
+
+    @Override
+    public ItemStack getUnavailIcon(String objective) {
+        String[] args = splitObjective(objective);
+        ObjectiveResolver resolver = getChild(args[TYPE]);
+        return resolver == null ? null : resolver.getUnavailIcon(args[PARAM]);
+    }
+
+    @NotNull
+    private String[] splitObjective(String objective) {
+        String[] args = objective.split(TYPE_SEPARATOR, 2);
+        if (args.length == 1) { // we need an empty string there if we want to avoid checking every time
+            return new String[]{args[TYPE], ""}; //there are cleaner ways to do this, but this is
+            // simpler when we only have a length of two
+        } else {
+            return args;
+        }
+    }
+}

bukkit/src/main/java/io/github/xxyy/common/util/inventory/InventoryHelper.java
@@ -16,6 +16,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
@@ -43,6 +44,17 @@ public final class InventoryHelper {
      * Default is 4 rows of slots.
      */
     public static final int DEFAULT_MAX_INVENTORY_SIZE = SLOTS_PER_ROW * 4;
+    /**
+     * Maximum amount of items allowed in armor slots
+     */
+    public static final int ARMOR_SIZE = 4;
+    /**
+     * Default amount of slots in a player inventory
+     *
+     * @see InventoryType#PLAYER
+     */
+    public static final int PLAYER_INV_SIZE = InventoryType.PLAYER.getDefaultSize();
+
     public static final String ACTION_PREFIX_PLACE = "PLACE_";
     public static final String ACTION_PREFIX_HOTBAR = "HOTBAR_";
     public static final String ACTION_PREFIX_DROP = "DROP_";

commit 2eed5ef4479e7f69fce65ae4ad99eec7b3ea090a

Fix compilation errors introduced in ba57271

games/src/main/java/io/github/xxyy/common/games/kits/Kit.java
@@ -12,7 +12,6 @@ package io.github.xxyy.common.games.kits;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.NotNull;
 
 import io.github.xxyy.common.util.inventory.InventoryHelper;
 
@@ -221,7 +220,7 @@ public class Kit implements Comparable<Kit> {
     }
 
     @Override
-    public int compareTo(@NotNull Kit other) {
+    public int compareTo(Kit other) {
         if (this.equals(other)) {
             return 0;
         }

games/src/main/java/io/github/xxyy/common/games/kits/objective/ParentObjectiveResolver.java
@@ -12,7 +12,6 @@ package io.github.xxyy.common.games.kits.objective;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -112,7 +111,6 @@ public class ParentObjectiveResolver implements ObjectiveResolver {
         return resolver == null ? null : resolver.getUnavailIcon(args[PARAM]);
     }
 
-    @NotNull
     private String[] splitObjective(String objective) {
         String[] args = objective.split(TYPE_SEPARATOR, 2);
         if (args.length == 1) { // we need an empty string there if we want to avoid checking every time