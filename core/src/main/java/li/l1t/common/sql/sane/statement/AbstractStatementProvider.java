f2204f8e49c993a11b8202a74b816431d98b1268
xxyy <devnull@nowak-at.net>
2016-10-20 21:09:39 +0200

    Fix typo in AbstractStatementProvider causing SQL params to be ignored

--- core/src/main/java/li/l1t/common/sql/sane/statement/AbstractStatementProvider.java
+++ core/src/main/java/li/l1t/common/sql/sane/statement/AbstractStatementProvider.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ * Copyright (c) 2013 - 2016 xxyy (Philipp Nowak; xyc@l1t.li). All rights reserved.
  *
  * Any usage, including, but not limited to, compiling, running, redistributing, printing,
  *  copying and reverse-engineering is strictly prohibited without explicit written permission
  *  from the original author and may result in legal steps being taken.
  *
- * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ * See the included LICENSE file (core/src/main/resources) for details.
  */
 
 package li.l1t.common.sql.sane.statement;
@@ -58,7 +58,7 @@ abstract class AbstractStatementProvider implements StatementProvider {
 
     @NotNull
     private PreparedStatement fillStatementRaw(@Nonnull PreparedStatement statement, @Nonnull Object[] parameters) throws SQLException {
-        for (int i = 0; 1 < parameters.length; i++) {
+        for (int i = 0; i < parameters.length; i++) {
             setStatementParameter(statement, i + 1, parameters[i]);
         }
         return statement;
--- lanatus/sql/src/main/java/li/l1t/lanatus/sql/account/JdbcAccountFetcher.java
+++ lanatus/sql/src/main/java/li/l1t/lanatus/sql/account/JdbcAccountFetcher.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ * Copyright (c) 2013 - 2016 xxyy (Philipp Nowak; xyc@l1t.li). All rights reserved.
  *
  * Any usage, including, but not limited to, compiling, running, redistributing, printing,
  *  copying and reverse-engineering is strictly prohibited without explicit written permission
  *  from the original author and may result in legal steps being taken.
  *
- * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ * See the included LICENSE file (core/src/main/resources) for details.
  */
 
 package li.l1t.lanatus.sql.account;
@@ -57,7 +57,8 @@ class JdbcAccountFetcher<T extends LanatusAccount> extends AbstractJdbcFetcher<T
     private QueryResult executeSql(String whereClause, Object... parameters) throws DatabaseException {
         return sql().query(
                 buildSelect(whereClause),
-                parameters);
+                parameters
+        );
     }
 
     @Override
