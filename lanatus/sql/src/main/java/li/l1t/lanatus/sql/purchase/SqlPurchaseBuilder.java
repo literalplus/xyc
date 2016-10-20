67527f281be975bb8f9a27a57b18306a530d7650
xxyy <devnull@nowak-at.net>
2016-10-20 23:24:00 +0200

    Fix SqlPurchaseBuilder failing if player account doesn't exist

--- lanatus/sql/src/main/java/li/l1t/lanatus/sql/purchase/SqlPurchaseBuilder.java
+++ lanatus/sql/src/main/java/li/l1t/lanatus/sql/purchase/SqlPurchaseBuilder.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ * Copyright (c) 2013 - 2016 xxyy (Philipp Nowak; xyc@l1t.li). All rights reserved.
  *
  * Any usage, including, but not limited to, compiling, running, redistributing, printing,
  *  copying and reverse-engineering is strictly prohibited without explicit written permission
  *  from the original author and may result in legal steps being taken.
  *
- * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ * See the included LICENSE file (core/src/main/resources) for details.
  */
 
 package li.l1t.lanatus.sql.purchase;
@@ -78,10 +78,11 @@ public class SqlPurchaseBuilder implements PurchaseBuilder, LanatusConnected, Sq
     public void build() throws IllegalStateException, NoSuchProductException, DatabaseException {
         checkNotYetExecuted();
         try (ScopedSession scoped = sql().scoped().tx()) {
+            makeSurePlayerAccountExists();
             Product product = findProduct();
             SqlPurchase purchase = createPurchase(product);
             purchaseWriter.write(purchase);
-            MutableAccount account = client().accounts().findMutable(playerId);
+            MutableAccount account = client().accounts().findMutable(playerId); //needs to exist for purchase
             account.modifyMelonsCount(purchase.getMelonsCost() * -1);
             client().accounts().save(account);
             client().positions().createFromPurchase(purchase);
@@ -89,6 +90,13 @@ public class SqlPurchaseBuilder implements PurchaseBuilder, LanatusConnected, Sq
             this.purchase = purchase; //don't mess up hasBeenBuilt() if a method throws an exception above
         } catch (AccountConflictException e) {
             throw new DatabaseException(e);
+        }
+    }
+
+    private void makeSurePlayerAccountExists() throws AccountConflictException {
+        MutableAccount account = client().accounts().findMutable(playerId);
+        if (account.getInitialState().isDefault()) {
+            client().accounts().save(account);
         }
     }
 
