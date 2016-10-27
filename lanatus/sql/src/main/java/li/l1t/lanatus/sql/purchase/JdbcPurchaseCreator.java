c5fde33e48a7e7cdf87037f7205a5e5ff412441f
xxyy <devnull@nowak-at.net>
2016-10-27 23:27:22 +0200

    Fix incorrect Instant retrieval in JdbcPurchaseCreator
    Would cause AbstractmethodError

--- lanatus/sql/src/main/java/li/l1t/lanatus/sql/purchase/JdbcPurchaseCreator.java
+++ lanatus/sql/src/main/java/li/l1t/lanatus/sql/purchase/JdbcPurchaseCreator.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ * Copyright (c) 2013 - 2016 xxyy (Philipp Nowak; xyc@l1t.li). All rights reserved.
  *
  * Any usage, including, but not limited to, compiling, running, redistributing, printing,
  *  copying and reverse-engineering is strictly prohibited without explicit written permission
  *  from the original author and may result in legal steps being taken.
  *
- * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ * See the included LICENSE file (core/src/main/resources) for details.
  */
 
 package li.l1t.lanatus.sql.purchase;
@@ -18,7 +18,6 @@ import li.l1t.lanatus.sql.common.AbstractJdbcEntityCreator;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.Instant;
 
 /**
  * Creates purchase objects from JDBC SQL result sets.
@@ -37,7 +36,7 @@ class JdbcPurchaseCreator extends AbstractJdbcEntityCreator<Purchase> {
     public Purchase createFromCurrentRow(ResultSet rs) throws SQLException {
         return new SqlPurchase(
                 uuid(rs, "id"), uuid(rs, "player_uuid"),
-                findProduct(rs), rs.getObject("created", Instant.class),
+                findProduct(rs), rs.getTimestamp("created").toInstant(),
                 rs.getString("data"), rs.getString("comment"), rs.getInt("melonscost")
         );
     }
