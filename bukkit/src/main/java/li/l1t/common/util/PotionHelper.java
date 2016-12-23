c078799e2d710a1d7531f41ef96ece2f438ee5f1
Literallie <devnull@nowak-at.net>
2016-12-23 11:23:34 +0100

    Add PotionData methods to PotionHelper

--- bukkit/src/main/java/li/l1t/common/util/PotionHelper.java
+++ bukkit/src/main/java/li/l1t/common/util/PotionHelper.java
@@ -12,10 +12,13 @@ package li.l1t.common.util;
 
 import com.google.common.base.Preconditions;
 import org.bukkit.Color;
+import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
 
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,7 +45,8 @@ public class PotionHelper {
      *
      * @param spec the specifier string for the potion effect to create
      * @return the potion effect parsed from given spec
-     * @throws IllegalArgumentException if given spec does not match the format outlined above
+     * @throws IllegalArgumentException if given spec does not match the format outlined above or the potion type does
+     *                                  not exist
      */
     public static PotionEffect effectFromString(String spec) {
         Matcher matcher = EFFECT_SPEC_PATTERN.matcher(spec);
@@ -51,7 +55,7 @@ public class PotionHelper {
         }
         String effectTypeName = matcher.group(1);
         PotionEffectType effectType = PotionEffectType.getByName(effectTypeName);
-        Preconditions.checkNotNull(effectType, "unknown effect type : %s", effectTypeName, spec);
+        Preconditions.checkArgument(effectType != null, "unknown effect type : %s", effectTypeName, spec);
         int amplifier = findIntIgnoringFirstCharacter(matcher.group(2)).orElse(0);
         int duration = findIntIgnoringFirstCharacter(matcher.group(3)).orElse(Integer.MAX_VALUE);
         return createEffect(effectType, amplifier, duration);
@@ -86,5 +90,58 @@ public class PotionHelper {
      */
     public static String stringFromEffect(PotionEffect effect) {
         return String.format("%s:%d*%d", effect.getType().getName(), effect.getAmplifier(), effect.getAmplifier());
+    }
+
+    /**
+     * Creates a potion data from a string specifying type, duration, and amplifier. The effects are matched using
+     * {@link PotionType#valueOf(String)}. <p> Specs look like this: {@code EFFECT.up.ext} </p> <p> Only EFFECT is
+     * required, all other parameters may be omitted. The parameters specify whether the potion is upgraded in power and
+     * has extended duration, respectively. Therefore, the following strings are all valid: <ul> <li>{@code SPEED} ->
+     * Speed I with default duration</li> <li>{@code SPEED.up} -> Speed II with default duration</li>
+     * <li>{@code SPEED.up.ext} -> Speed II with extended duration</li> <li>{@code SLOW_DIGGING.ext} -> Mining
+     * Fatigue II with default duration</li> </ul> </p>
+     *
+     * @param spec the specifier string for the potion data to create
+     * @return the potion data parsed from given spec
+     * @throws IllegalArgumentException if given spec does not match the format outlined above or the potion type does
+     *                                  not exist
+     */
+    public static PotionData dataFromString(String spec) {
+        Preconditions.checkNotNull(spec, "spec");
+        String[] parts = spec.split("\\.");
+        PotionType type = PotionType.valueOf(spec.toUpperCase().replace("[- ]", "_"));
+        boolean extended = false;
+        boolean upgraded = false;
+        for (int i = 1; i < parts.length; i++) {
+            String part = parts[i];
+            if (part.equalsIgnoreCase("up")) {
+                upgraded = true;
+            } else if (part.equalsIgnoreCase("ext")) {
+                extended = true;
+            } else {
+                throw new IllegalArgumentException("unparseable data: " + part + "; " + spec);
+            }
+        }
+        return new PotionData(type, extended, upgraded);
+    }
+
+    /**
+     * Creates a string from a potion data that may be passed back to {@link #dataFromString(String)} (String)} to
+     * create given data.
+     *
+     * @param data the data to serialise into a string
+     * @return the string representing given data
+     */
+    public static String stringFromData(PotionData data) {
+        StringBuilder sb = new StringBuilder(data.getType().name());
+        appendIf(sb, data, PotionData::isUpgraded, ".up");
+        appendIf(sb, data, PotionData::isExtended, ".ext");
+        return sb.toString();
+    }
+
+    private static <T> void appendIf(StringBuilder sb, T param, Predicate<? super T> condition, String text) {
+        if (condition.test(param)) {
+            sb.append(text);
+        }
     }
 }
