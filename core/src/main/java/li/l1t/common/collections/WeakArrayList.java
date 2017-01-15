058f1b9176152ca3e502dc05fbf08f7b25d70589
Literallie <git@l1t.li>
2017-01-15 22:45:01 +0100

    Remove unnecessary //TODO comments from WeakArrayList
    won't get implemented, class is deprecated
    (cherry picked from commit f8a2994)

--- core/src/main/java/li/l1t/common/collections/WeakArrayList.java
+++ core/src/main/java/li/l1t/common/collections/WeakArrayList.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2013 - 2015 xxyy (Philipp Nowak; devnull@nowak-at.net). All rights reserved.
+ * Copyright (c) 2013 - 2017 xxyy (Philipp Nowak; xyc@l1t.li). All rights reserved.
  *
  * Any usage, including, but not limited to, compiling, running, redistributing, printing,
  *  copying and reverse-engineering is strictly prohibited without explicit written permission
  *  from the original author and may result in legal steps being taken.
  *
- * See the included LICENSE file (core/src/main/resources) or email xxyy98+xyclicense@gmail.com for details.
+ * See the included LICENSE file (core/src/main/resources) for details.
  */
 
 package li.l1t.common.collections;
@@ -218,19 +218,19 @@ public final class WeakArrayList<E> implements List<E> {
     @Override
     @Nonnull
     public ListIterator<E> listIterator() {
-        throw new UnsupportedOperationException(); //TODO implement
+        throw new UnsupportedOperationException();
     }
 
     @Override
     @Nonnull
     public ListIterator<E> listIterator(final int index) {
-        throw new UnsupportedOperationException(); //TODO implement
+        throw new UnsupportedOperationException();
     }
 
     @Override
     @Nonnull
     public List<E> subList(final int fromIndex, final int toIndex) {
-        throw new UnsupportedOperationException(); //TODO implement
+        throw new UnsupportedOperationException();
     }
 
 }
