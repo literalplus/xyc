02896e8b000739e138e42f3e11f0e820534ce6c5
Literallie <devnull@nowak-at.net>
2017-03-07 21:01:45 +0100

    Add task scheduling utility methods to XyPluggable

--- bukkit/src/main/java/li/l1t/common/xyplugin/XyPluggable.java
+++ bukkit/src/main/java/li/l1t/common/xyplugin/XyPluggable.java
@@ -10,6 +10,8 @@
 
 package li.l1t.common.xyplugin;
 
+import li.l1t.common.version.PluginVersion;
+
 /**
  * Common interface for plugins interfacing with XYC using the {@link AbstractXyPlugin} API.
  */
@@ -19,4 +21,23 @@ public interface XyPluggable {
     void disable();
 
     void enable();
+
+    PluginVersion getPluginVersion();
+
+    /**
+     * Convenience shorthand for scheduling an asynchronous task with the server's scheduler for immediate execution.
+     *
+     * @param task the task to execute
+     * @see #serverThread(Runnable) (Runnable) for scheduling in the server thread
+     */
+    void async(Runnable task);
+
+    /**
+     * Convenience shorthand for scheduling an task with the server's scheduler for immediate execution in the server
+     * thread.
+     *
+     * @param task the task to execute
+     * @see #async(Runnable) for async scheduling
+     */
+    void serverThread(Runnable task);
 }
