a00712d8e20c1366ebe436c03541389f629eb211
Literallie <devnull@nowak-at.net>
2017-04-03 00:44:09 +0200

    Add Utf8Control for UTF-8 ResourceBundles
    (cherry picked from commit d71d6c3)

---
+++ core/src/main/java/li/l1t/common/i18n/Utf8Control.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013 - 2017 xxyy (Philipp Nowak; xyc@l1t.li). All rights reserved.
+ *
+ * Any usage, including, but not limited to, compiling, running, redistributing, printing,
+ *  copying and reverse-engineering is strictly prohibited without explicit written permission
+ *  from the original author and may result in legal steps being taken.
+ *
+ * See the included LICENSE file (core/src/main/resources) for details.
+ */
+
+package li.l1t.common.i18n;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+public class Utf8Control extends ResourceBundle.Control {
+    public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
+            throws IllegalAccessException, InstantiationException, IOException {
+        // below is basically a copy of the default implementation that just uses UTF-8
+        String bundleName = toBundleName(baseName, locale);
+        String resourceName = toResourceName(bundleName, "properties");
+        InputStream stream = null;
+        if (reload) {
+            URL url = loader.getResource(resourceName);
+            if (url != null) {
+                URLConnection connection = url.openConnection();
+                if (connection != null) {
+                    connection.setUseCaches(false);
+                    stream = connection.getInputStream();
+                }
+            }
+        } else {
+            stream = loader.getResourceAsStream(resourceName);
+        }
+        if (stream != null) {
+            try (InputStreamReader reader = new InputStreamReader(stream, "UTF-8")) {
+                return new PropertyResourceBundle(reader);
+            }
+        }
+        return null;
+    }
+}
